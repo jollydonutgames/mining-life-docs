c4dc53a7428ca985c53f29bc2152d94517d5dc59
jollydonutgames <108772994+jollydonutgames@users.noreply.github.com>
2022-10-17 00:28:34 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'Mining Life Docs'
+copyright = u'2022, Jolly Donut Games LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
